e3f6692c576d841eff23da86b189bfb69a74d4b8
Spctr <oliver.matter01@gmail.com>
2021-12-17 22:09:52 +0100

    set right .info()

--- 03_function/example.js
+++ 03_function/example.js
@@ -7,6 +7,6 @@ function ausgabe(vorname) {
 var nachname = 'Sonjachen';                                                     // definieren 'nachname'
 var laenge = nachname.length;                                                   // definieren Länge von 'nachname' als laenge
 var vorname = ausgabe(nachname);                                                // definieren 'vorname' über function(ausgabe) inklusive String
-console.info('Anzahl Buchstaben in Sonja: ' + laenge);                          // Ausgabe Länge von 'nachname' in der Console
+console.info('Anzahl Buchstaben in Sonja: ', laenge);                          // Ausgabe Länge von 'nachname' in der Console
 console.log('Inhalt der Variable außerhalb der Funktion: ' + nachname)          // Ausgabe 'nachname' direkt ausserhalb der function
 console.log('Inhalt der Variable vorname außerhalb der Funktion: '+ vorname)    // Ausgabe 'nachname' über function(ausgabe)
